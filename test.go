81c9fb3bfad78f26931ac5ee02aa7d5e17eb7169
Masashi Kimura <screw@mtg.biglobe.ne.jp>
2016-09-28 14:01:54 +0900

    簡易wikiに作り直し

--- test.go
+++ test.go
@@ -1,30 +1,59 @@
 package main
 
 import (
+	"html/template"
+	"io/ioutil"
 	"net/http"
-	"text/template"
 )
 
 type page struct {
 	Title string
-	Count int
+	Body  []byte
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	http.ListenAndServe(":80", nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	pg := page{"Hello, World", 1}
-	//tmpl, err := template.New("new").Parse("{{.Title}} {{.Count}} count")
-	tmpl, err := template.ParseFiles("layout.html")
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[6:]
+	body := r.FormValue("message")
+	p := &page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[6:]
+	p, err := load(title)
 	if err != nil {
-		panic(err)
+		p = &page{Title: title}
+	}
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[6:]
+	p, _ := load(title)
+	t, _ := template.ParseFiles("layout.html")
+	t.Execute(w, p)
+}
+
+func (p *page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func load(title string) (*page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
 	}
 
-	err = tmpl.Execute(w, pg)
-	if err != nil {
-		panic(err)
-	}
+	return &page{Title: title, Body: body}, nil
 }
